503c4ba7bcf980bdc0401dbf06b376429dca6f80
Saeid Alizadeh <s.alizadeh1989@yahoo.com>
2019-01-17 23:39:52 +0330

    finaly i can or not!

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("what can i do for you baby this is my first code that i wrote in my pc .tanks of mc load")
+
+	foo()
+
+	for i:=0 ;i<100 ;i++  {
+		fmt.Println(i)
+	}
+}
+
+func foo(){
+	fmt.Println("this is for that you learn how to use two funk ")
+}
